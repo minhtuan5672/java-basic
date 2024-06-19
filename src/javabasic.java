c71e279b045eb6e6bc17a240a8b0d778869fd128
minh tuan <minhtuan5672@gmail.com>
2024-06-19 15:40:57 +0900

    fiest commit

---
+++ src/javabasic.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class hellojava {
+    public static void main(String[] args) {
+//        bfjk
+        /*
+        vgv
+        ko doc code
+        */
+        System.out.println("hello word");
+        System.out.println("welcome to java");
+        System.out.println(4 + 5);
+
+        Scanner scanne = new Scanner(System.in);
+        System.out.println(scanne.nextLine);
+    }
+}
